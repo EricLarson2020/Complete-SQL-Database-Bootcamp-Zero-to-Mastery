07b32372b10587ae230ae57516004945058fd7c0
Eric Larson <ericlarsonbroom@gmail.com>
2021-03-10 19:32:30 -0700

    Update exercises.sql

--- SQL Deep Dive/Aggregate Functions/exercises.sql
+++ SQL Deep Dive/Aggregate Functions/exercises.sql
@@ -8,10 +8,12 @@
 
 -- Question 1: What is the average salary for the company?
 -- Table: Salaries
+63810.744836143706
 
 
 -- Question 2: What year was the youngest person born in the company?
 -- Table: employees
+1965
 
 
 -- BEFORE YOU START
@@ -23,7 +25,7 @@
 
 -- Question 1: How many towns are there in france?
 -- Table: Towns
-
+36684
 
 -- BEFORE YOU START
 /*
@@ -34,10 +36,10 @@
 
 -- Question 1: How many official languages are there?
 -- Table: countrylanguage
-
+238
 -- Question 2: What is the average life expectancy in the world?
 -- Table: country
-
+66.48603611164265
 -- Question 3: What is the average population for cities in the netherlands?
 -- Table: city
 
