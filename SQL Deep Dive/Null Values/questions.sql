b5ec3443f6e4cc8ced69755e02355495d27e0c7a
Eric Larson <ericlarsonbroom@gmail.com>
2021-03-20 21:38:00 -0600

    Update questions.sql

--- SQL Deep Dive/Null Values/questions.sql
+++ SQL Deep Dive/Null Values/questions.sql
@@ -4,9 +4,12 @@
 * Question: 
 * Assuming a students minimum age for the class is 15, what is the average age of a student?
 */
-
+SELECT AVG(COALESCE(AGE, 15)) FROM "Student";
+30.4
 /*
 * DB: https://www.db-fiddle.com/f/PnGNcaPYfGoEDvfexzEUA/0
 * Question: 
 * Replace all empty first or last names with a default?
 */
+
+SELECT id, coalesce(name, 'default') AS firstName, coalesce(lastName, 'another_default') AS lastName from "Student";
