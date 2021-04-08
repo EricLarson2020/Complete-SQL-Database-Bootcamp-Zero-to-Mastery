e5efcf774fa1843b215412b092699cb238d104e3
Eric Larson <ericlarsonbroom@gmail.com>
2021-04-07 21:24:09 -0600

    Update questions.sql

--- SQL Deep Dive/Like Operator/questions.sql
+++ SQL Deep Dive/Like Operator/questions.sql
@@ -5,7 +5,9 @@
 * Sample output: https://imgur.com/vXs4093
 * Use EXTRACT (YEAR FROM AGE(birth_date)) we will learn about this in later parts of the course
 */
-SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
+SELECT emp_no, EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees
+WHERE first_name ILIKE 'M%';
+                                              
 
 
 /*
@@ -14,6 +16,9 @@ SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
 * Question: How many people's name start with A and end with R?
 * Expected output: 1846
 */
+                                      
+SELECT COUNT(emp_no) FROM employees
+WHERE first_name ILIKE 'A%R';
 
                                                   
 /*
@@ -22,15 +27,17 @@ SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
 * Question: How many people's zipcode have a 2 in it?.
 * Expected output: 4211 
 */
-
-
-
+                                      
+SELECT COUNT(customerId) FROM customers
+WHERE zip::text ILIKE '%2%';
 /*
 * DB: Store
 * Table: customers
 * Question: How many people's zipcode start with 2 with the 3rd character being a 1.
 * Expected output: 109 
 */
+SELECT COUNT(customerId) FROM customers
+WHERE CAST(zip AS text) ILIKE '2_1%';
 
 
 /*
@@ -40,4 +47,6 @@ SELECT ..., EXTRACT (YEAR FROM AGE(birth_date)) as "age" FROM employees;
 * Replace null values with "No State"                                                  
 * Expected output: https://imgur.com/AVe6G4c
 */
+SELECT state, COALESCE(null, 'No State') FROM customers
+WHERE CAST(phone AS text) ILIKE '302%';
 
